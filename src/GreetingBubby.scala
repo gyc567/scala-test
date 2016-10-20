5defc5e4cb30ad98db0bbc5192bef665478c94e6
eric567 <gyc567@126.com>
2016-10-20 08:09:06 +0800

    add the new code for example

--- src/GreetingBubby.scala
+++ src/GreetingBubby.scala
@@ -1,3 +1,6 @@
+import java.util.{Date, Locale}
+import java.text.DateFormat
+import java.text.DateFormat._
 /**
   * Created by eric567 [email:gyc567@126.com] 
   * on 10/18/2016.
@@ -5,5 +8,8 @@
 object GreetingBubby {
   def main(args: Array[String]): Unit = {
     println("hello world")
+    val now = new Date
+    val df = getDateInstance(LONG, Locale.CHINA)
+    println(df format now)
   }
 }
